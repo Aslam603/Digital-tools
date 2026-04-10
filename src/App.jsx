a27b77d8bf93d82fbb5f78869f00b48b33d36b54
Shelby1920 <aslamanik28@gmail.com>
2026-04-11 01:37:24 +0600

    made banner section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/navBar'
+import Banner from './components/banner'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,8 @@ function App() {
     <>
       
 <NavBar></NavBar>
+
+<Banner></Banner>
 
 
     </>
---
+++ src/components/banner.jsx
@@ -0,0 +1,53 @@
+import BannerImage from "../assets/banner.png";
+
+const Banner = () => {
+  return (
+    <div className="relative min-h-162.5 flex items-center overflow-hidden">
+      <div className="mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center relative z-10">
+        
+        {/* Left Content */}
+        <div className="space-y-4">
+          <div className="inline-flex items-center gap-2 bg-blue-300 text-blue-600 text-sm font-medium px-5 py-2 rounded-full">
+            New: AI-Powered Tools Available
+          </div>
+
+          <h1 className="text-4xl lg:text-5xl font-bold leading-tight tracking-tighter text-black">
+            Supercharge Your 
+            <br />
+            <span>Digital Workflow</span>
+          </h1>
+
+          <p className="text-lg text-zinc-700 max-w-lg">
+            Access premium AI tools, design assets, templates, and productivity
+            software—all in one place. Start creating faster today.
+          </p>
+
+          <div className="flex flex-wrap gap-4 pt-4">
+            <button className="bg-linear-to-r from-blue-600 to-purple-600 text-white px-5 py-2.5 rounded-3xl">
+              Explore Products
+            </button>
+            <button className="border border-blue-600 text-blue-600 rounded-2xl px-3.5 py-2">
+              Watch Demo
+            </button>
+          </div>
+
+          <div className="h-12 w-px bg-white"></div>
+        </div> {/* End Left Content */}
+
+        {/* Right Content / Image */}
+        <div className="relative flex justify-center lg:justify-end">
+          <div className="relative">
+            <img
+              className="relative h-130 w-auto object-contain drop-shadow-2xl rounded-3xl animate-pulse"
+              src={BannerImage}
+              alt="AI Models Banner"
+            />
+          </div>
+        </div>
+
+      </div> 
+    </div> 
+  );
+};
+
+export default Banner;
